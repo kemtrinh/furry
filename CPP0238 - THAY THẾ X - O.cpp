38182bf6eea61417295dd393f9fff7174b1854a1
kemtrinh <144366949+kemtrinh@users.noreply.github.com>
2024-11-20 21:19:21 +0700

    Add files via upload

--- CPP0238 - THAY THẾ X - O.cpp
+++ CPP0238 - THAY THẾ X - O.cpp
@@ -1,65 +1,77 @@
-// Created by Nguyễn Mạnh Quân
-
-#include<bits/stdc++.h>
-
+#include<iostream>
 using namespace std;
+int main() {
+	int t; cin >> t;
+	while (t--) {
+		char M[1000][1000];
+		int m, n; cin >> m >> n;
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				cin >> M[i][j];
+			}
+		}
+		for (int i = 0; i < n; i++)
+			if (M[0][i] == 'O')M[0][i] = '8';
+		for (int i = 0; i < m; i++)
+			if (M[i][n - 1] == 'O')M[i][n - 1] = '8';
+		for (int i = n - 1; i >= 0; i--)
+			if (M[m - 1][i] == 'O') M[m - 1][i] = '8';
+		for (int i = m - 1; i >= 0; i--)
+			if (M[i][0] == 'O' )M[i][0] = '8';
+		
+		for (int i = 1; i < m-1; i++) {
+			for (int j = 1; j < n-1; j++) {
+				if (M[i][j] == 'O'&&( M[i - 1][j] == '8' || M[i][j - 1] == '8' || M[i][j + 1] == '8')) {
+					for (int l = j; j < n; l++)if (M[i][l] == 'O')M[i][l] = '8'; else break;
+					for (int l = j - 1; l >= 0; l--)if (M[i][l] == 'O')M[i][l] = '8'; else break;
+				}
+			}
+		}
+		for (int i = 0; i < m/2; i++) {
+			for (int j = 0; j < n; j++)swap(M[i][j], M[m - i - 1][j]);
+		}
+		for (int i = 1; i < m - 1; i++) {
+			for (int j = 1; j < n - 1; j++) {
+				if (M[i][j] == 'O' && (M[i - 1][j] == '8' || M[i][j - 1] == '8' || M[i][j + 1] == '8')) {
+					for (int l = j; j < n; l++)if (M[i][l] == 'O')M[i][l] = '8'; else break;
+					for (int l = j - 1; l >= 0; l--)if (M[i][l] == 'O')M[i][l] = '8'; else break;
+				}
+			}
+		}
+		for (int i = 0; i < m / 2; i++) {
+			for (int j = 0; j < n; j++)swap(M[i][j], M[m - i - 1][j]);
+		}
+		for (int i = 0; i< n - 1; i++) {
+			for (int j = 1; j < m - 1; j++) {
+				if (M[j][i] == 'O' && (M[j + 1][i] == '8' || M[j][i + 1] == '8' || M[j - 1][i] == '8')) {
+					for (int l = j; l < m; l++)if (M[l][i] == 'O')M[l][i] = '8'; else break;
+					for (int l = j-1; l >=0; l--)if (M[l][i] == 'O')M[l][i] = '8'; else break;
+				}
+			}
+		}
+		for (int i = 0; i < n / 2; i++) {
+			for (int j = 0; j < m; j++)swap(M[j][i], M[n - 1 - j][i]);
+		}
+		for (int i = 0; i < n - 1; i++) {
+			for (int j = 1; j < m - 1; j++) {
+				if (M[j][i] == 'O' && (M[j + 1][i] == '8' || M[j][i + 1] == '8' || M[j - 1][i] == '8')) {
+					for (int l = j; l < m; l++)if (M[l][i] == 'O')M[l][i] = '8'; else break;
+					for (int l = j - 1; l >= 0; l--)if (M[l][i] == 'O')M[l][i] = '8'; else break;
+				}
+			}
+		}
+		for (int i = 0; i < n / 2; i++) {
+			for (int j = 0; j < m; j++)swap(M[j][i], M[n - 1 - j][i]);
+		}
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				if (M[i][j] == 'O')cout << 'X' << " ";
+				else if (M[i][j] == '8')cout << 'O' << " ";
+				else cout << 'X' << " ";
+			}
+			cout << endl;
+		}
+		
 
-#define mp make_pair
-#define fi first
-#define se second
-#define pb push_back
-#define sz size()
-#define ll long long
-#define FOR(i, a, b) for(int i = a; i <= b; ++i)
-#define FORD(i, a, b) for(int i = a; i >= b; --i)
-#define F(i, a, b) for(int i = a; i < b; ++i)
-#define FD(i, a, b) for(int i = a; i > b; --i)
-#define faster() ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
-#define vi vector<int>
-#define vll vector<ll>
-#define vb vector<bool>
-#define endl '\n'
-
-int n, m;
-char a[405][405];
-
-void Fill(int x, int y, char Cur, char New)
-{
-    if (x < 0 || x >= n || y < 0 || y >= m) return;
-    if (a[x][y] != Cur) return;
-    a[x][y] = New;
-    Fill(x + 1, y, Cur, New);
-    Fill(x - 1, y, Cur, New);
-    Fill(x, y + 1, Cur, New);
-    Fill(x, y - 1, Cur, New);
+	}
 }
-
-void Solve()
-{
-   F(i, 0, n) F(j, 0, m) if(a[i][j] == 'O') a[i][j] = '-';
-   F(i, 0, n) if(a[i][0] == '-') Fill(i, 0, '-', 'O');
-   F(i, 0, n) if(a[i][m - 1] == '-') Fill(i, m - 1, '-', 'O');
-   F(i, 0, m) if(a[0][i] == '-') Fill(0, i, '-', 'O');
-   F(i, 0, m) if(a[n - 1][i] == '-') Fill(n - 1, i, '-', 'O');
-   F(i, 0, n) F(j, 0, m) if(a[i][j] == '-') a[i][j] = 'X';
-}
-
-int main()
-{
-    faster();
-    int t;
-    cin >> t;
-    while(t--)
-    {
-        cin >> n >> m;
-        F(i, 0, n) F(j, 0, m) cin >> a[i][j];
-        Solve();
-        F(i, 0, n)
-        {
-            F(j, 0, m) cout << a[i][j] << ' ';
-            cout << endl;
-        }
-    }
-    return 0;
-}
-
